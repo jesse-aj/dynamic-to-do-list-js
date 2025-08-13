b619c1030c245d297b8544770f82c7dd33971e9f
jesse-aj <appiahjesse123@gmail.com>
2025-08-13 05:50:32 +0000

    Created element to store and remove list items from the tasks

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
  
 const addButton = document.getElementById("add-task-btn");
 const taskInput = document.getElementById("task-input");
@@ -6,11 +6,28 @@ const taskList  =  document.getElementById("task-list");
 
 function addTask () {
 
- const taskText = taskInput.ariaValueMax.trim();
- if (taskText === ""){
+ const taskInput = taskInput.ariaValueMax.trim();
+ if (taskInput === ""){
     alert("Please enter a task.")
  }
 }
+
+const listItem = document.createElement("li");
+listItem.textContent = taskInput;
+
+//This will create a remove button and remove <li> from the list and also provide new space for input task
+
+const removeButton = document.createElement("button");
+removeButton.textContent = "Remove";
+removeButton.className = "remove-btn";
+removeButton.onclick = function() {
+    taskList.removeChild(listItem);
+};
+
+listItem.appendChild(removeButton);
+taskList.appendChild(listItem);
+taskInput.value = "";
+
 
 
 
