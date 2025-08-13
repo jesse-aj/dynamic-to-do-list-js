27e41df791f2be335073b15eeea1f2123a517cd5
jesse-aj <appiahjesse123@gmail.com>
2025-08-13 05:30:39 +0000

    Added the function that takes input of the tasks and trim it to store in a constant container

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const addButton = document.getElementById("add-task-btn");
 const taskInput = document.getElementById("task-input");
 const taskList  =  document.getElementById("task-list");
 
+function addTask () {
 
+ const taskText = taskInput.ariaValueMax.trim();
+ if (taskText === ""){
+    alert("Please enter a task.")
+ }
+}
 
 
 
