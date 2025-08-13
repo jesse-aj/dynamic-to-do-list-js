b46f2359118fcb655f7650ab19f682241159a1be
jesse-aj <appiahjesse123@gmail.com>
2025-08-13 14:38:43 +0000

    added code that allows a new tasks added to the page and saved in local storage immediately

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
 const addButton = document.getElementById("add-task-btn");
 const taskInput = document.getElementById("task-input");
 const taskList  =  document.getElementById("task-list");
- // Loading tasks from localStorage part
 
- 
+ // Loading tasks from localStorage part
 const savedTasks = localStorage.getItem("tasks");
 
 if(savedTasks) {
@@ -18,9 +17,6 @@ if(savedTasks) {
     });
 }
 
-
-
-
 function addTask () {
 
  const taskText = taskInput.value.trim();
@@ -30,8 +26,8 @@ function addTask () {
     return;
  }
 
-//Created a new element <li> to store and display tasks on the page, it was stored in the taskInput container
 
+//Created a new element <li> to store and display tasks on the page, it was stored in the taskInput container
 const listItem = document.createElement("li");
 listItem.textContent = taskText;
 
@@ -39,16 +35,23 @@ listItem.textContent = taskText;
 const removeButton = document.createElement("button");
 removeButton.textContent = "Remove";
 removeButton.classList.add = "remove-btn";
+
 //A function : This shows when a the remove button is clicked it should remove <li> from the list
 removeButton.onclick = function() {
     taskList.removeChild(listItem);
 };
+
 // Appent the remove button to the <li> : this adds a remove button to the list after a task has been added
  listItem.appendChild(removeButton);
 
  //This adds the whole <li> : which now includes the button into the <ul> called taskList.
 
 taskList.appendChild(listItem);
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+tasks.push(taskText);
+localStorage.setItem("tasks", JSON.stringify(tasks));
+
 
 //This clear the input feild for the next task
 taskInput.value = "";
